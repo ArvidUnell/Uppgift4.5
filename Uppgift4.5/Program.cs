84802f65cc5b52b527cbe872235a9fc7870e6f86
arun06001 <arun06001@E-PW0234KE>
2023-10-18 13:09:59 +0200

    Add project files.

---
+++ Uppgift4.5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace Uppgift4_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett heltal");
+            string tal = Console.ReadLine();
+
+            string svar = "";           
+
+            for (int i = 0;i < tal.Length;i++)
+            {
+                byte siffra = byte.Parse(tal[i].ToString());
+
+                if (siffra == 9)
+                {
+                    siffra = 0;
+                }
+                else
+                {
+                    siffra += 1;
+                }
+
+                svar += siffra;
+            }
+
+            Console.WriteLine(svar);
+
+            Console.WriteLine("Tryck på en valfri knapp för att avsluta");
+            Console.ReadKey();
+        }
+    }
+}
